83dd7cabc00e33c5f45f528a25a944752e2ef8e1
exize1 <amio121212@gmail.com>
2022-11-04 17:53:42 +0200

    adding-enviroments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 import { useState } from 'react';
 import Home from './2.Home';
 
-export const socket = io.connect("http://localhost:3001")
+export const socket = io.connect(process.env.REACT_APP_SERVER_URL)
 
 function App() {
 
